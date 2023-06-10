39f94c2689619e47e742a62987b020448153fb99
Matheus Sousa Dias <75400361+L4TN@users.noreply.github.com>
2023-06-10 17:06:26 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,150 @@
+import { checkGameOver, getCell, getCoordinates } from "./getcheck.js";
+import {
+    highlightCells,
+    highlightOccupiedCells,
+    isCellHighlighted,
+    removeHighlight,
+} from "./highlight.js";
+import {
+    isJaguar,
+    isPiece,
+    isValidMove,
+    jaguarEat,
+    movePiece,
+    selectPiece,
+} from "./pieces.js";
+import { cells, selectedPiece } from "./variables.js";
+/*
+
+
+
+*/
+function onInitGame() {
+    cells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            if (isPiece(cell)) {
+                selectPiece(cell);
+                highlightCells();
+                highlightOccupiedCells();
+            } else if (selectedPiece) {
+                const [selectedX, selectedY] = getCoordinates(selectedPiece);
+                const [currentX, currentY] = getCoordinates(cell);
+
+                var cellAtual = getCell(currentX, currentY);
+                var cellProx = getCell(selectedX, selectedY);
+
+                if (
+                    isJaguar(selectedPiece) &&
+                    !isValidMove(cellAtual, cellProx)
+                ) {
+                    jaguarEat(selectedPiece,cellAtual);
+                    removeHighlight();
+                }
+
+                if (
+                    isValidMove(cellAtual, cellProx) &&
+                    !isPiece(cell) &&
+                    isCellHighlighted(cell)
+                ) {
+                    movePiece(cell);
+                    removeHighlight();
+                }
+            }
+        });
+    });
+}
+
+onInitGame();
+
+window.addEventListener("load", function () {
+    const loadingScreen = document.getElementById("loading-screen");
+    const mainContent = document.getElementById("main-content");
+
+    // Animação de desaparecimento
+    loadingScreen.style.transition = "opacity 1s ease-in-out";
+
+    // Após 5 segundos, comece a animação de desaparecimento
+    setTimeout(() => {
+        loadingScreen.style.opacity = "0";
+    }, 2000);
+
+    // Após a animação de desaparecimento, remova a tela de carregamento e mostre o conteúdo principal
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+        mainContent.style.display = "block";
+    }, 3000); // Este tempo deve ser a soma do tempo de espera antes da animação iniciar e a duração da animação
+});
+
+setInterval(function () {
+    checkGameOver();
+}, 1000);
+
+var audio = new Audio("music.mp3");
+audio.volume = 0.3;
+
+document.getElementById("play-button").addEventListener("click", function () {
+    audio.play();
+});
+
+document.getElementById("mute-Button").addEventListener("click", function () {
+    if (audio.muted) {
+        audio.muted = false;
+    } else {
+        audio.muted = true;
+    }
+});
+
+document.querySelectorAll(".piece-jaguar").forEach((element) => {
+    element.addEventListener("click", () => {
+        const gif = document.getElementById("myGif");
+        gif.style.display = "block";
+
+        setTimeout(() => {
+            gif.style.display = "none";
+        }, 2000);
+    });
+});
+
+// Função para verificar novo movimento
+function checkForNewMove() {
+    fetch('https://adugo-game-backend-01.onrender.com/api/check')
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'move available') {
+          console.log('Novo movimento disponível:', data.move);
+          // Atualizar o tabuleiro aqui com o novo movimento recebido
+          updateBoard(data.move);
+        } else {
+          console.log('Nenhum novo movimento disponível');
+                  //print o status do erro
+            
+        }
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro ao verificar o novo movimento:', error);
+                //print o status do erro
+      });
+  }
+
+  function updateBoard(newMove) {
+    Array.from(cells).forEach((cell) => {
+      const cellX = parseInt(cell.getAttribute("data-x"));
+      const cellY = parseInt(cell.getAttribute("data-y"));
+  
+      const move = newMove.find((move) => {
+        const x = parseInt(move.x);
+        const y = parseInt(move.y);
+        return x === cellX && y === cellY;
+      });
+  
+      if (move) {
+        const classNames = move.classList.join(" ");
+        cell.className = "cell " + classNames;
+      }
+    });
+
+    highlightCells();
+  }
+    
+// Chamar a função checkForNewMove a cada 1 segundo
+setInterval(checkForNewMove, 50); // 1 segundo
---
+++ js/pieces.js
@@ -0,0 +1,267 @@
+import { removeHighlight } from "./highlight.js";
+import { getCoordinates, getCell, checkAllDirections } from "./getcheck.js";
+import {
+    jaguarSound,
+    modifySelectedPiece,
+    positions,
+    selectedPiece,
+    modifyPieacesEatenbyJaguar,
+    pieacesEatenbyJaguar,
+    cells,
+} from "./variables.js";
+/*
+
+*/
+function isPiece(cell) {
+    return (
+        cell.classList.contains("piece-dog") ||
+        cell.classList.contains("piece-jaguar")
+    );
+}
+
+function selectPiece(piece) {
+    if (isJaguar(piece)) {
+        jaguarSound.play();
+    }
+    modifySelectedPiece(piece);
+}
+
+function isJaguar(cell) {
+    return cell.classList.contains("piece-jaguar");
+}
+
+function isDog(cell) {
+    return cell.classList.contains("piece-dog");
+}
+
+function isValidMove(cellAtual, cellProx) {
+    const [selectedX, selectedY] = getCoordinates(cellAtual);
+    const [currentX, currentY] = getCoordinates(cellProx);
+
+    // transformando 'positions' num objeto para facilitar a pesquisa
+    const positionsMap = positions.reduce((acc, [key, value]) => {
+        acc[key] = value;
+        return acc;
+    }, {});
+
+    // convertendo coordenadas para string
+    const currentPos = `${selectedX}.${selectedY}`;
+    const nextPos = `${currentX}.${currentY}`;
+
+    // verificando se 'nextPos' está nas posições adjacentes de 'currentPos'
+    if (
+        positionsMap[currentPos] &&
+        positionsMap[currentPos].includes(nextPos)
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
+function isValidJaguarMove(selectedX, selectedY, currentX, currentY) {
+    return (
+        Math.abs(currentX - selectedX) === 2 &&
+        Math.abs(currentY - selectedY) === 2
+    );
+}
+
+function onPieceEatenByJaguar() {
+    // Atualiza o placar
+    document.getElementById("score").innerText = pieacesEatenbyJaguar;
+}
+
+function movePiece(cell) {
+    console.log("movePiece()");
+    cell.classList.add(selectedPiece.classList[1]);
+    selectedPiece.classList.remove(selectedPiece.classList[1]);
+    modifySelectedPiece(null);
+
+    let cellsArray = Array.from(cells);
+    console.log(cellsArray);
+    sendTabletop(cellsArray);
+}
+
+function jaguarEat(jaguarPiece,cellAtual) {
+    let x = parseInt(jaguarPiece.getAttribute("data-x"));
+    let y = parseInt(jaguarPiece.getAttribute("data-y"));
+    let cellPosEliminationAfter;
+    let cellsAround = checkAllDirections(x, y);
+
+    let listCellPieceinJaguar = cellsAround.filter(
+        (cell) => cell && cell.classList.contains("piece-dog")
+    );
+
+    for (let i = 0; i < listCellPieceinJaguar.length; i++) {
+        let piece = listCellPieceinJaguar[i];
+        let pieceX = parseInt(piece.getAttribute("data-x"));
+        let pieceY = parseInt(piece.getAttribute("data-y"));
+
+        if (pieceY === y && pieceX < x) {
+            cellPosEliminationAfter = getCell(pieceX - 1, pieceY);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog") && cellPosEliminationAfter === cellAtual
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        } else if (pieceY === y && pieceX > x) {
+            cellPosEliminationAfter = getCell(pieceX + 1, pieceY);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog") && cellPosEliminationAfter === cellAtual
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        } else if (pieceX === x && pieceY < y) {
+            cellPosEliminationAfter = getCell(pieceX, pieceY - 1);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog") && cellPosEliminationAfter === cellAtual
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        } else if (pieceX === x && pieceY > y) {
+            cellPosEliminationAfter = getCell(pieceX, pieceY + 1);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog") && cellPosEliminationAfter === cellAtual
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        }
+        // Direções diagonais
+        else if (pieceX > x && pieceY > y) {
+            // Diagonal inferior direita
+            cellPosEliminationAfter = getCell(pieceX + 1, pieceY + 1);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog") && cellPosEliminationAfter === cellAtual
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        } else if (pieceX < x && pieceY < y) {
+            // Diagonal superior esquerda
+            cellPosEliminationAfter = getCell(pieceX - 1, pieceY - 1);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog")
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        } else if (pieceX > x && pieceY < y) {
+            // Diagonal superior direita
+            cellPosEliminationAfter = getCell(pieceX + 1, pieceY - 1);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog") && cellPosEliminationAfter === cellAtual
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        } else if (pieceX < x && pieceY > y) {
+            // Diagonal inferior esquerda
+            cellPosEliminationAfter = getCell(pieceX - 1, pieceY + 1);
+            if (
+                cellPosEliminationAfter &&
+                !cellPosEliminationAfter.classList.contains("piece-dog")
+            ) {
+                piece.classList.remove("piece-dog");
+                movePiece(cellPosEliminationAfter);
+                modifyPieacesEatenbyJaguar(pieacesEatenbyJaguar + 1);
+                //pieacesEatenbyJaguar = pieacesEatenbyJaguar + 1;
+                onPieceEatenByJaguar();
+                removeHighlight();
+            }
+        }
+    }
+    let cellsArray = Array.from(cells);
+    console.log(cellsArray);
+
+    sendTabletop(cellsArray);
+}
+
+
+
+async function sendTabletop(aux) {
+    // Mapeia o NodeList para um array de objetos
+    let cellArray = aux.map((cell, index) => {
+        return {
+            classList: Array.from(cell.classList),
+            x: cell.getAttribute("data-x"),
+            y: cell.getAttribute("data-y")
+        };
+    });
+
+    // Transforma o array em uma string JSON
+    let serialized = JSON.stringify(cellArray);
+    console.log(serialized);
+    
+    const response = await fetch('https://adugo-game-backend-01.onrender.com/api/send-move', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cellArray),
+      });
+    
+      if (!response.ok) {
+        console.error(`HTTP error! status: ${response.status}`);
+      } else {
+        console.log("Move sent successfully");
+      }
+    
+      if (!response.ok) {
+        console.error(`HTTP error! status: ${response.status}`);
+      } else {
+        console.log("Move sent successfully");
+      }
+    
+}
+
+
+export {
+    isDog,
+    isJaguar,
+    isPiece,
+    isValidJaguarMove,
+    isValidMove,
+    movePiece,
+    selectPiece,
+    onPieceEatenByJaguar,
+    jaguarEat,
+};
